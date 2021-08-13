e5da6314e90c6b832b27ba36ded99dee9176bb33
JuanTenjo <tenjo2001@gmail.com>
2021-08-13 07:35:30 -0500

    Agrege componente GesionUserApp

---
+++ Frontend/src/Components/UserComponents/gestionUserApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GestionUserApp = () => {
+    return ( 
+        <div>
+            <h1>Gestion User</h1>
+        </div>
+     );
+}
+ 
+export default GestionUserApp;
--- Frontend/src/Routes/Routes.js
+++ Frontend/src/Routes/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {HashRouter, Redirect, Route, Switch} from "react-router-dom"
 import LoginApp from '../Pages/PageLogin';
 import Home from '../Pages/PageHome';
+import GestionUserApp from '../Components/UserComponents/gestionUserApp';
 import PrivateRoute from './PrivateRoute';
 const Routes = () => {
 
@@ -10,6 +11,7 @@ const Routes = () => {
         <div>
             <HashRouter>
                 <Switch>
+                    <PrivateRoute exact path='/GestionUser' component={GestionUserApp} />
                     <PrivateRoute exact path='/Home' component={Home} />
                     <Route exact path='/IniciarSesion' component={LoginApp} />
                     <Route exact path='/'>
